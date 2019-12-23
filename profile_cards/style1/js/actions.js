6d28f3e9c5382777e2730b3d45a6ff56029ae53e
dtkcs01 <dtkcs01@gmail.com>
2019-12-23 21:15:13 +0530

    init

---
+++ profile_cards/style1/js/actions.js
@@ -0,0 +1,27 @@
+var showMenu = function() {
+  $('.menu-icon').hide(500);
+  $("#menu").show(500);
+};
+
+var hideMenu = function() {
+  $('.menu-icon').show(500);
+  $("#menu").hide(500);
+};
+
+var showContact = function() {
+  $('#contact').show(500);
+};
+
+var hideContact = function() {
+  $('#contact').hide(500);
+};
+
+var showSocials = function() {
+  $('.drop-icon').hide(500);
+  $('#socials').show(500);
+};
+
+var hideSocials = function() {
+  $('.drop-icon').show(500);
+  $('#socials').hide(500);
+};
